3b15ddd1958b0bfa469ed61f7b17db51e9d78791
Atharva Gupta <atharva.gu@gmail.com>
2021-07-09 18:18:50 -0500

    MeetingsPageUpdate1

--- SquareOne/ViewController.swift
+++ SquareOne/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MeetingButton: UIButton!
     override func viewDidLoad() {
+        MeetingButton.layer.cornerRadius = 8
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func newMeetingPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "newMeeting", sender: self)
+    }
 }
+
+
 
