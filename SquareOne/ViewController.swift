d5e5fa423156017df8a41835a34c61d1f64283f4
Atharva Gupta <atharva.gu@gmail.com>
2021-07-09 15:55:58 -0500

    Initial Commit

---
+++ SquareOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SquareOne
+//
+//  Created by Atharva Gupta on 7/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
